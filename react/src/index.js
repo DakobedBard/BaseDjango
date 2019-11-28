7f06cf36211bccf867fd65d6a50075911ecb11d1
JasonHeyward <mddarr@gmail.com>
2019-11-28 07:36:07 -0800

    define and add a compionent

--- react/src/index.js
+++ react/src/index.js
@@ -6,3 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+class Car extends React.Component {
+    render(){
+        return <h2> Hi, I am a car </h2>;
+    }
+}
+
+ReactDOM.render(<Car /> , document.getElementById('car'));
